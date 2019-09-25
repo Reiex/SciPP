f176feb1c99c2d26585d0954b67740f32c839c9b
Reiex <reiex.marius38@gmail.com>
2019-09-25 07:34:53 +0200

    Prototypes points et forces simuPhysique

--- src/simuPhysique.h
+++ src/simuPhysique.h
@@ -3,6 +3,36 @@
 #include "SciPP.h"
 
 
+class Point;
+
+class Force
+{
+	public:
+
+		Vecteur<double> operator()(Point p, double t, double dt);
+
+	private:
+
+		Vecteur<double> (*m_func)(Point, double, double);
+};
+
+class Point
+{
+	public:
+
+		Point(double x, double y, double m);
+
+	private:
+
+		double m_x;
+		double m_y;
+		double m_m;
+		double m_dx;
+		double m_dy;
+		std::vector<Force> m_forces;
+};
+
+
 void simuDispersionVorticite(int Nx = 100, int Nt = 500, long double kappa = 0.01, long double t_simu = 7.5);
 
 void simuFluide2D(int Nx = 60, int Ny = 60, long double t_simu = 7.5, long double nu = 0.0001, long double rho = 30, long double delta = 0.05, long double Lx = 1, long double Ly = 1);
