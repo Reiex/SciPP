15056096f9ab07ff46c07b87aa9a836b234ad83a
Reiex <reiex.marius38@gmail.com>
2019-11-12 12:12:50 +0100

    Calcul des dérivées aux bords de smoothCruve

--- src/Plot.cpp
+++ src/Plot.cpp
@@ -874,14 +874,6 @@ Vect<Vect<long double>> getSmoothCurve(Vect<long double> const& x, Vect<long dou
 	if (n < 2)
 		return m;
 
-	// Courbe tangeante aux bords du polygone de contrôle
-
-	m[0][0] = (1-c)*(x[1] - x[0]);
-	m[1][0] = (1-c)*(y[1] - y[0]);
-
-	m[0][n-1] = (1-c)*(x[n-1] - x[n-2]);
-	m[1][n-1] = (1-c)*(y[n-1] - y[n-2]);
-
 	// Calcul des dérivées au milieu de la courbe
 
 	for (int i(1); i < n-1; i++)
@@ -889,6 +881,14 @@ Vect<Vect<long double>> getSmoothCurve(Vect<long double> const& x, Vect<long dou
 		m[0][i] = (1-c)*(x[i+1] - x[i-1])/2;
 		m[1][i] = (1-c)*(y[i+1] - y[i-1])/2;
 	}
+
+	// Courbe tangeante aux bords du polygone de contrôle
+
+	m[0][0] = 0;
+	m[1][0] = 0;
+
+	m[0][n-1] = 0;
+	m[1][n-1] = 0;
 
 	return m;
 }
