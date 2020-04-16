b2ec8ce1bb06545bd632100582e10ac69a10c74e
Reiex <reiex.marius38@gmail.com>
2020-04-16 10:01:01 +0200

    Implémentation listes

--- src/SciPP/Containers/Liste.hpp
+++ src/SciPP/Containers/Liste.hpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <exception>
+#include <initializer_list>
+
 
 template<typename T> class List
 {
@@ -15,6 +17,9 @@ template<typename T> class List
 		};
 
 		List();
+		List(unsigned int n);
+		List(std::initializer_list<T> tab);
+		List(T const* tab, unsigned int taille);
 		List(List<T> const& l);
 		List(List<T>&& l);
 
@@ -27,11 +32,14 @@ template<typename T> class List
 		void append(T const& x);
 		void remove(unsigned int i);
 
+		List<T>& operator+=(List<T> const& l);
+
 		~List();
 
 	private:
 		
 		void liberer();
+		void incAllocSize();
 
 		T* m_x;
 		unsigned int m_n;
@@ -48,6 +56,45 @@ template<typename T> const char* List<T>::IndexException::what() const throw()
 template<typename T> List<T>::List() : m_n(0), m_x(nullptr)
 {
 
+}
+
+template<typename T> List<T>::List(unsigned int n) : List()
+{
+	if (n != 0)
+	{
+		m_n = n;
+		while (m_n > m_allocSize)
+			incAllocSize();
+		m_x = new T[m_allocSize];
+		for (int i(0); i < m_n; i++)
+			m_x[i] = T()
+	}
+}
+
+template<typename T> List<T>::List(std::initializer_list<T> tab) : List()
+{
+	if (tab.size() > 0)
+	{
+		m_n = tab.size();
+		while (m_n > m_allocSize)
+			incAllocSize();
+		m_x = new T[m_allocSize];
+		for (int i(0); i < m_n; i++)
+			m_x[i] = tab.begin()[i];
+	}
+}
+
+template<typename T> List<T>::List(T const* tab, unsigned int taille) : List()
+{
+	if (taille > 0)
+	{
+		m_n = taille;
+		while (m_n > m_allocSize)
+			incAllocSize();
+		m_x = new T[m_allocSize];
+		for (int i(0); i < m_n; i++)
+			m_x[i] = tab[i];
+	}
 }
 
 template<typename T> List<T>::List(List<T> const& l) : List()
@@ -112,7 +159,7 @@ template<typename T> void List<T>::append(T const& x)
 	m_n += 1;
 	if (m_n > m_allocSize)
 	{
-		m_allocSize = m_allocSize == 0 ? 32 : m_allocSize * 2;
+		incAllocSize();
 		T* oldX(m_x);
 		m_x = new T[m_allocSize];
 		if (oldX != nullptr)
@@ -138,6 +185,36 @@ template<typename T> void List<T>::remove(unsigned int i)
 }
 
 
+template<typename T> List<T>& List<T>::operator+=(List<T> const& l)
+{
+	if (m_n + l.m_n > m_allocSize)
+	{
+		while (m_n + l.m_n > m_allocSize)
+			incAllocSize();
+		T* oldX(m_x);
+		m_x = new T[m_allocSize];
+		if (oldX != nullptr)
+		{
+			for (int i(0); i < m_n - 1; i++)
+				m_x[i] = oldX[i];
+			delete[] oldX;
+		}
+	}
+
+	for (int i(0); i < l.m_n; i++)
+		m_x[m_n + i] = l.m_x[i];
+	m_n += l.m_n;
+}
+
+template<typename T> List<T> operator+(List<T> const& a, List<T> const& b)
+{
+	List<T> c(a);
+	c += b;
+
+	return c;
+}
+
+
 template<typename T> List<T>::~List()
 {
 	liberer();
@@ -153,4 +230,26 @@ template<typename T> void List<T>::liberer()
 		m_n = 0;
 		m_allocSize = 0;
 	}
+}
+
+template<typename T> void List<T>::incAllocSize()
+{
+	m_allocSize = m_allocSize == 0 ? 32 : m_allocSize * 2;
+}
+
+
+template<typename T> std::ostream& operator<<(std::ostream& stream, Liste<T> const& l)
+{
+	stream << "[";
+	for (int i(0); i < l.size(); i++)
+	{
+		if (i != l.size() - 1)
+			stream << l[i] << ", ";
+		else
+			stream << l[i];
+	}
+
+	stream << "]";
+
+	return stream;
 }
