6f78819febdb2ecd7c2e2f721a71aaa3267a03ca
Reiex <reiex.marius38@gmail.com>
2020-04-24 23:46:05 +0200

    Suppression example inutile

--- src/examples/examples.cpp
+++ src/examples/examples.cpp
@@ -9,13 +9,12 @@ int main()
 {
 	// std::cout << neper(100) << std::endl << std::endl; system("pause");
 	// simuDispersionVorticite(100, 1000);
-	simuFluide2D(150, 150, 5);
+	// simuFluide2D(64, 64, 5);
 	// lancerInterpreteur();
 	// simuBalle();
 	// integrationFlot();
 	// fonctionLogistique();
 	// mandelbrot(-0.76, 0.1, 100); // mandelbrot(0.33, 0.552, 1000);
-	// dispertionChaleur2D(100, 100, 0.1);
 	// conway();
 
 	return 0;
