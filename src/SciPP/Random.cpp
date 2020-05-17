4ae9062a4864ca34d0c16eedf07c2518c7f00a1d
Reiex <reiex.marius38@gmail.com>
2020-05-17 03:49:07 +0200

    Ajout de bornes et renommage des fonctions de rand pour les int et les doubles

--- src/SciPP/Random.cpp
+++ src/SciPP/Random.cpp
@@ -1,6 +1,6 @@
 #include "Random.h"
 
-uint16_t scp::randint()
+uint16_t scp::randInt(uint16_t min, uint16_t max)
 {
 	static uint64_t seed(0), a(1103515245), c(12345);
 
@@ -10,26 +10,26 @@ uint16_t scp::randint()
 	uint64_t m(1);
 	seed = (a * seed + c) % (m << 31);
 
-	return seed >> 15;
+	return min + (seed >> 15) % (max - min);
 }
 
-long double scp::rand()
+long double scp::randDouble(long double min, long double max)
 {
-	uint64_t x(randint());
-	x = (x << 16) + randint();
-	x = (x << 16) + randint();
-	x = (x << 16) + randint();
+	uint64_t x(randInt());
+	x = (x << 16) + randInt();
+	x = (x << 16) + randInt();
+	x = (x << 16) + randInt();
 
-	return ((long double) x)/uint64_t(-1);
+	return min + ((long double)x/uint64_t(-1))*(max - min);
 }
 
 long double scp::normalRand(long double mean, long double sigma)
 {
-	long double a(rand()*2 - 1), b(rand()*2 - 1), w(a*a + b*b);
+	long double a(randDouble(-1, 1)), b(randDouble(-1, 1)), w(a*a + b*b);
 	while (w >= 1)
 	{
-		a = rand()*2 - 1;
-		b = rand()*2 - 1;
+		a = randDouble(-1, 1);
+		b = randDouble(-1, 1);
 		w = a*a + b*b;
 	}
 
--- src/SciPP/Random.h
+++ src/SciPP/Random.h
@@ -6,7 +6,7 @@
 
 namespace scp
 {
-	uint16_t randint();
-	long double rand();
+	uint16_t randInt(uint16_t min=0, uint16_t max=-1);
+	long double randDouble(long double min=0.0, long double max=1.0);
 	long double normalRand(long double mean=0.0, long double sigma=1.0);
 }
