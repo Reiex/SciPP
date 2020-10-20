c03ea7064b7cd427a3c0f0fc7a78aa42d090d3b8
Reiex <reiex.marius38@gmail.com>
2020-10-20 13:51:00 +0200

    Réorganisation générale

--- tests/main.cpp
+++ tests/main.cpp
@@ -8,5 +8,7 @@ void printSection(const std::string& testName, const std::string& sectionName)
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  int exit_code = RUN_ALL_TESTS();
+  system("pause");
+  return exit_code;
 }
